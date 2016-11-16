a6f020e06ab1fc23a8d9d482e57683185bf0d343
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2016-11-16 18:37:25 +0100

    It works!

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.register('/sw.js').then(function(registration) {
+//     // Registration was successful
+//     console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//   }).catch(function(err) {
+//     // registration failed :(
+//     console.log('ServiceWorker registration failed: ', err);
+//   });
+// }
+
+(async() => {
+  try {
+    const outputArea = document.querySelector('.rss-container');
+    const fetchURL = 'http://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fpolitiken.dk%2Frss%2Findland.rss',
+    fetchOptions = {
+      method: 'get'
+    };
+    const response = await fetch(fetchURL, fetchOptions);
+    const data = await response.json();
+    console.log(data);
+    for (var i = 0; i < data.items.length; i++) {
+      outputArea.innerHTML += `
+        <a class="news-item" href="${data.items[i].link}">
+          <h2>${data.items[i].title}</h2>
+          <p>${data.items[i].content}</p>
+        </a>
+      `;
+    }
+  } catch (e) {
+    console.log('ERROR: ' + e);
+  }
+})();
